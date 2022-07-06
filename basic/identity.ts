4db41d6e1a893a1130975338a6b6095a775e71df
3hliuj <1902644369@qq.com>
2022-07-06 17:38:08 +0800

    泛型

--- basic/identity.ts
+++ basic/identity.ts
@@ -8,3 +8,73 @@ function identity<T>(arg: T): T {
 // K（Key）：表示对象中的键类型；
 // V（Value）：表示对象中的值类型；
 // E（Element）：表示元素类型。
+
+// 泛型约束
+
+// function identity1<T>(arg: T): T {
+//   return arg.size; // Error: Property 'size' does not exist on type 'T'
+// }
+
+interface Sizeable {
+  size: number;
+}
+// 使用 extends 关键字可以做到这一点。简单来说就是你定义一个类型，然后让 T 实现这个接口即可
+function identity1<T extends Sizeable>(arg: T): T {
+  console.log(arg.size);
+  return arg;
+}
+
+// typeof
+
+interface Person {
+  name: string;
+  age: number;
+}
+const sem: Person = { name: "semlinker", age: 30 };
+type Sem = typeof sem; // type Sem = Person
+
+// 在上面代码中，我们通过 typeof 操作符获取 sem 变量的类型并赋值给 Sem 类型变量，之后我们就可以使用 Sem 类型：
+
+const lolo: Sem = { name: "lolo", age: 5 }
+
+// keyof ,该操作符可以用于获取某种类型的所有键，其返回类型是联合类型。
+interface Person1{
+  name:string
+  age:number
+}
+
+
+type K1 = keyof Person; // "name" | "age"
+type K2 = keyof Person[]; // "length" | "toString" | "pop" | "push" | "concat" | "join" 
+type K3 = keyof { [x: string]: Person };  // string | number
+
+
+type Todo = {
+  id: number;
+  text: string;
+  done: boolean;
+}
+
+const todo: Todo = {
+  id: 1,
+  text: "Learn TypeScript keyof",
+  done: false
+}
+
+function prop<T extends object, K extends keyof T>(obj: T, key: K) {
+  return obj[key];
+}
+
+const id = prop(todo, "id"); // const id: number
+const text = prop(todo, "text"); // const text: string
+const done = prop(todo, "done"); // const done: boolean
+
+// const date=prop(todo,'date') // Argument of type '"date"' is not assignable to parameter of type '"id" | "text" | "done"'.
+
+// in 用来遍历枚举类型
+
+type Keys = "a" | "b" | "c"
+
+type Obj =  {
+  [p in Keys]: any
+} // -> { a: any, b: any, c: any }
